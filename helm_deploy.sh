a3091b5c1232ceedfd860c9c2505a88a7bc78cb0
10151940 <10151940liu_he@cn.tre-inc.com>
2023-01-30 14:55:53 +0800

    init project

---
+++ helm_deploy.sh
@@ -0,0 +1,6 @@
+# Get Credentials
+gcloud container clusters get-credentials "$CLOUDSDK_CONTAINER_CLUSTER" --zone "$CLOUDSDK_COMPUTE_ZONE" --project "$PROJECT_ID"
+# Upgrade charts
+helm upgrade --install \
+    -f "./ingress-nginx/values.yaml" cicd-testing-env \
+    ./ingress-nginx
