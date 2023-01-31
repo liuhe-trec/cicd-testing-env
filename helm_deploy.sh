e200d860e4f99cde35d57a332bd1cc4614eb73a9
10151940 <10151940liu_he@cn.tre-inc.com>
2023-01-31 11:20:45 +0800

    Add vue project

--- helm_deploy.sh
+++ helm_deploy.sh
@@ -1,6 +1,7 @@
 # Get Credentials
 gcloud container clusters get-credentials "$CLOUDSDK_CONTAINER_CLUSTER" --zone "$CLOUDSDK_COMPUTE_ZONE" --project "$PROJECT_ID"
 # Upgrade charts
-helm upgrade --install \
-    -f "./ingress-nginx/values.yaml" nginx \
-    ./ingress-nginx --namespace ingress-nginx --create-namespace
+helm upgrade -f "./ingress-nginx/values.yaml" nginx \
+    ./ingress-nginx --install --namespace ingress-nginx --create-namespace
+helm upgrade -f "./cicd-testing-charts/values.yaml" cicd-testing-vue \
+    ./cicd-testing-charts -- install
