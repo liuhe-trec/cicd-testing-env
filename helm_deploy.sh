d4fd06fc93bc6dad42855286fac89cf46aa238f6
10151940 <10151940liu_he@cn.tre-inc.com>
2023-01-31 11:26:04 +0800

    modify

--- helm_deploy.sh
+++ helm_deploy.sh
@@ -4,4 +4,4 @@ gcloud container clusters get-credentials "$CLOUDSDK_CONTAINER_CLUSTER" --zone "
 helm upgrade -f "./ingress-nginx/values.yaml" nginx \
     ./ingress-nginx --install --namespace ingress-nginx --create-namespace
 helm upgrade -f "./cicd-testing-charts/values.yaml" cicd-testing-vue \
-    ./cicd-testing-charts -- install
+    ./cicd-testing-charts --install
